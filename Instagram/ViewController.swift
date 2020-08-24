d9da6d4808c66fa22b3d0f30880f0fd0df144cbd
Anthony Kim <anthony092777@gmail.com>
2020-08-24 16:48:18 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Anthony Kim on 8/24/20.
+//  Copyright © 2020 Anthony Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
