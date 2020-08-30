0df523a0f55b3b77a47fe1334383d6a8914bad04
Anthony Kim <anthony092777@gmail.com>
2020-08-30 03:19:08 -0400

    update login and register view controller

--- Instagram/Resources/AuthManager.swift
+++ Instagram/Resources/AuthManager.swift
@@ -14,12 +14,60 @@ public class AuthManager {
     
     // MARK: - Public
     
-    public func registerNewUser(username: String, email: String, password: String) {
-        
+    public func registerNewUser(username: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
+        /*
+         - Check if username is available
+         - Check if email is available
+         */
+
+        DatabaseManager.shared.canCreateNewUser(with: email, username: username) { canCreate in
+            if canCreate{
+                /*
+                 - Create account
+                 - Insert account to database
+                 */
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    guard error == nil, result != nil else{
+                        // Firebase auth could not create account
+                        completion(false)
+                        return
+                    }
+                    // insert into database
+                    DatabaseManager.shared.insertNewUser(with: email, username: username) { inserted in
+                        if inserted {
+                            completion(true)
+                            return
+                        }else{
+                            // Failed to insert to database
+                            completion(false)
+                            return
+                        }
+                        
+                    }
+                }
+            }else{
+                // either username or email does not exist
+                completion(false)
+            }
+        }
     }
     
-    public func loginUser(username: String?, email: String?, password: String){
-        
+    // escaping bc we used completion inside of another closure and as a result, the scope needs to escape
+    public func loginUser(username: String?, email: String?, password: String, completion: @escaping (Bool) -> Void){
+        // figureout if the user is logging in with username or email
+        if let email = email{
+            // email log in
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error  in
+                guard authResult != nil, error == nil else{
+                    completion(false)
+                    return
+                }
+                completion(true)
+            }
+        }else if let user = username {
+            // username log in
+            print(username)
+        }
     }
     
 }
--- Instagram/Resources/DatabaseManager.swift
+++ Instagram/Resources/DatabaseManager.swift
@@ -12,8 +12,45 @@ public class DatabaseManager {
     
     static let shared = DatabaseManager()
     
+    private let database = Database.database().reference()
+    
     // MARK: - Public
     
+    // functions that auth manager is going to user
+    
+    /// Check if username and email is available
+    /// - Paramters
+    ///     - email: String representing email
+    ///     - username: String representing username
+    public func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void) {
+        completion(true) // implement later
+    }
+    
+    /// Inserts new user data to database
+    /// - Paramters
+    ///     - email: String representing email
+    ///     - username: String representing username
+    ///     - completion: Async callback for result if database entry succeeded
+    public func insertNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
+        let key = email.safeDatabaseKey()
+   
+        database.child(key).setValue(["username": username]) { error, _ in
+            if error == nil {
+                // success
+                completion(true)
+                return
+            }else{
+                // failed
+                completion(false)
+                return
+            }
+            
+        }
+    }
+    
+    // MARK: Private
+    
+
    
     
 }
